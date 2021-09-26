7ff4415d3314b55a8a07a8a5074e984e172c3b3a
Christian Letter <christianletter@gmail.com>
2021-09-26 19:22:00 +0200

    feat(bookcard): component for book working properly, also with remove buttons

--- BookSwapClient/components/bookCard.js
+++ BookSwapClient/components/bookCard.js
@@ -1,35 +1,152 @@
-import React, { useState, useEffect } from 'react';
-import {
-  View,
-  Text,
-  ScrollView,
-  StyleSheet,
-  FlatList,
-  TouchableOpacity,
-  Image,
-  Button,
-  TextInput,
-} from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import AppLoading from 'expo-app-loading';
+import { IconButton, Colors } from 'react-native-paper';
 
-const bookCard = (props) => {
-  return (
-    <View>
-      {props.info?.userFrom === props.user ? (
-        <View>
-          {props.info?.hasBeenViewed ? <Text> notificatiooooon</Text> : null}
-          <Text>You made a request to the user {props.info?.userTo}</Text>
-        </View>
-      ) : (
-        <Text>
-          {!props.info?.hasBeenViewed ? <Text> notificatiooooon</Text> : null}
-          User {props.info?.userFrom} want your book
-        </Text>
-      )}
-      <TouchableOpacity onPress={props.pressing}>
-        <Text>See the details</Text>
-      </TouchableOpacity>
-    </View>
-  );
+import {
+  useFonts,
+  Rosario_300Light,
+  Rosario_400Regular,
+  Rosario_500Medium,
+  Rosario_600SemiBold,
+  Rosario_700Bold,
+  Rosario_300Light_Italic,
+  Rosario_400Regular_Italic,
+  Rosario_500Medium_Italic,
+  Rosario_600SemiBold_Italic,
+  Rosario_700Bold_Italic,
+} from '@expo-google-fonts/rosario';
+
+const BookCard = (props) => {
+  const [fontsLoaded] = useFonts({
+    Rosario_300Light,
+    Rosario_400Regular,
+    Rosario_500Medium,
+    Rosario_600SemiBold,
+    Rosario_700Bold,
+    Rosario_300Light_Italic,
+    Rosario_400Regular_Italic,
+    Rosario_500Medium_Italic,
+    Rosario_600SemiBold_Italic,
+    Rosario_700Bold_Italic,
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <View
+        /* I need this outerCard style because, the shadows were not working
+        on the LinearGradient, so I wrapped everything in a View */
+        style={styles.outerCard}
+      >
+        {props.removeBtn ? (
+          <TouchableOpacity>
+            <IconButton
+              style={styles.minusButton}
+              icon="minus"
+              color={Colors.white}
+              size={33}
+              onPress={() => props.removeBtn(props.bookObj.ISBN)}
+            />
+            <IconButton
+              style={styles.minusButton}
+              icon="minus-circle"
+              color={Colors.red500}
+              size={33}
+              onPress={() => props.removeBtn(props.bookObj.ISBN)}
+            />
+          </TouchableOpacity>
+        ) : null}
+        <LinearGradient
+          colors={['#A73FD3', '#5D3FD3']}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 1, y: 1 }}
+          style={styles.card}
+        >
+          {props.bookObj.thumbnail ? (
+            <View style={styles.imageContainer}>
+              <Image
+                source={{ uri: props.bookObj.thumbnail }}
+                style={styles.image}
+              />
+            </View>
+          ) : null}
+          <View style={styles.textContainer}>
+            <Text style={styles.textTitle}>{props.bookObj.title}</Text>
+            <Text style={styles.textAuthor}>{props.bookObj.authors}</Text>
+          </View>
+        </LinearGradient>
+      </View>
+    );
+  }
 };
 
-export default bookCard;
+const styles = StyleSheet.create({
+  outerCard: {
+    // position: 'relative',
+    shadowColor: 'black',
+    shadowOffset: { width: 1, height: 2 },
+    shadowOpacity: 0.9,
+    shadowRadius: 2,
+    elevation: 5,
+    zIndex: -5,
+  },
+  card: {
+    flexDirection: 'row',
+    marginTop: 20,
+    marginHorizontal: 15,
+    // margin: 15,
+    borderRadius: 5,
+    shadowColor: 'black',
+    shadowOffset: { width: 1, height: 2 },
+    shadowOpacity: 0.9,
+    shadowRadius: 2,
+    elevation: 5,
+    padding: 6,
+    zIndex: -5,
+  },
+  textContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    flexGrow: 2,
+  },
+  textTitle: {
+    color: 'white',
+    paddingTop: 30,
+    paddingBottom: 10,
+    fontSize: 20,
+    fontFamily: 'Rosario_500Medium',
+  },
+  textAuthor: {
+    color: 'white',
+    paddingHorizontal: 20,
+    fontSize: 15,
+    fontFamily: 'Rosario_300Light_Italic',
+  },
+  imageContainer: {
+    width: 90,
+    height: 144,
+    borderRadius: 5,
+    shadowColor: '#000',
+    shadowOffset: { width: 1, height: 2 },
+    shadowOpacity: 0.9,
+    shadowRadius: 2,
+    elevation: 5,
+    zIndex: -5,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 5,
+    zIndex: -5,
+  },
+  minusButton: {
+    position: 'absolute',
+    top: -11,
+    left: -13,
+    zIndex: 10,
+  },
+});
+
+export default BookCard;
--- BookSwapClient/screens/Library/UserLibrary.js
+++ BookSwapClient/screens/Library/UserLibrary.js
@@ -9,41 +9,15 @@ import {
 } from 'react-native';
 import { IconButton, Colors } from 'react-native-paper';
 import { useIsFocused } from '@react-navigation/native';
-import { LinearGradient } from 'expo-linear-gradient';
-import AppLoading from 'expo-app-loading';
-import {
-  useFonts,
-  Rosario_300Light,
-  Rosario_400Regular,
-  Rosario_500Medium,
-  Rosario_600SemiBold,
-  Rosario_700Bold,
-  Rosario_300Light_Italic,
-  Rosario_400Regular_Italic,
-  Rosario_500Medium_Italic,
-  Rosario_600SemiBold_Italic,
-  Rosario_700Bold_Italic,
-} from '@expo-google-fonts/rosario';
 
 import { UserContext } from '../../AuthContext';
 import BASE_URL from '../../configClient';
+import BookCard from '../../components/BookCard';
 
 const UserLibrary = ({ navigation }) => {
   const { user } = useContext(UserContext);
   const [books, setBooks] = useState(null);
   const isFocused = useIsFocused();
-  const [fontsLoaded] = useFonts({
-    Rosario_300Light,
-    Rosario_400Regular,
-    Rosario_500Medium,
-    Rosario_600SemiBold,
-    Rosario_700Bold,
-    Rosario_300Light_Italic,
-    Rosario_400Regular_Italic,
-    Rosario_500Medium_Italic,
-    Rosario_600SemiBold_Italic,
-    Rosario_700Bold_Italic,
-  });
 
   function fetchBookFromDb() {
     fetch(`${BASE_URL}/books/${user.id}/library`)
@@ -53,7 +27,7 @@ const UserLibrary = ({ navigation }) => {
   }
 
   useEffect(() => {
-    if (isFocused) fetchBookFromDb();
+    fetchBookFromDb();
   }, [isFocused]);
 
   function removeBook(isbn) {
@@ -67,73 +41,36 @@ const UserLibrary = ({ navigation }) => {
       )
       .then(() => fetchBookFromDb());
   }
-  if (!fontsLoaded) {
-    return <AppLoading />;
-  } else {
-    return (
-      <View style={styles.container}>
-        <FlatList
-          data={books}
-          keyExtractor={(item) => item.ISBN}
-          renderItem={({ item }) => (
-            <View style={styles.test}>
-              <LinearGradient
-                colors={['#A73FD3', '#5D3FD3']}
-                start={{ x: 0, y: 0 }}
-                end={{ x: 1, y: 1 }}
-                style={styles.wholeResult}
-              >
-                {item.thumbnail ? (
-                  <View
-                    style={{
-                      width: 90,
-                      height: 144,
-                      borderRadius: 10,
-                      shadowColor: '#000',
-                      shadowOffset: { width: 1, height: 2 },
-                      shadowOpacity: 0.9,
-                      shadowRadius: 2,
-                      elevation: 5,
-                    }}
-                  >
-                    <Image
-                      source={{ uri: item.thumbnail }}
-                      style={{
-                        width: '100%',
-                        height: '100%',
-                        borderRadius: 10,
-                      }}
-                    />
-                  </View>
-                ) : null}
-                <View style={styles.textContainer}>
-                  <Text style={styles.textTitle}>{item.title}</Text>
-                  <Text style={styles.textAuthor}>{item.authors}</Text>
-                  {/* <TouchableOpacity onPress={() => removeBook(item.ISBN)}>
-                <Text style={styles.text}>Remove the book</Text>
-              </TouchableOpacity> */}
-                </View>
-              </LinearGradient>
-            </View>
-          )}
-        />
-        <IconButton
-          style={styles.plusButton}
-          icon="plus"
-          color={Colors.white}
-          size={70}
-          onPress={() => navigation.navigate('Insert A New Book')}
-        />
-        <IconButton
-          style={styles.plusButton}
-          icon="plus-circle"
-          color={Colors.green500}
-          size={70}
-          onPress={() => navigation.navigate('Insert A New Book')}
-        />
-      </View>
-    );
-  }
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={books}
+        keyExtractor={(item) => item.ISBN}
+        renderItem={({ item }) => (
+          <BookCard bookObj={item} removeBtn={() => removeBook(item.ISBN)} />
+        )}
+      />
+      <IconButton
+        /* here I am inserting two buttons since the one that I want to use has a
+          cross in the middle, which is not filled with any colors, therefore the
+          background is visible in the middle of the button. I placed a similar white
+          button underneath the one that I want to use, so that it covers the background*/
+        style={styles.plusButton}
+        icon="plus"
+        color={Colors.white}
+        size={65}
+        onPress={() => navigation.navigate('Insert A New Book')}
+      />
+      <IconButton
+        style={styles.plusButton}
+        icon="plus-circle"
+        color={Colors.green500}
+        size={65}
+        onPress={() => navigation.navigate('Insert A New Book')}
+      />
+    </View>
+  );
 };
 
 export default UserLibrary;
@@ -143,54 +80,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'white',
   },
-  gradientContainer: {
-    flex: 1,
-    shadowColor: 'black',
-    shadowOffset: { width: 1, height: 2 },
-    shadowOpacity: 0.9,
-    shadowRadius: 2,
-    elevation: 5,
-  },
   plusButton: {
     position: 'absolute',
     top: '85%',
     left: '76%',
-  },
-  test: {
-    shadowColor: 'black',
-    shadowOffset: { width: 1, height: 2 },
-    shadowOpacity: 0.9,
-    shadowRadius: 2,
-    elevation: 5,
-  },
-  wholeResult: {
-    flexDirection: 'row',
-    margin: 10,
-    borderRadius: 10,
-    shadowColor: 'black',
-    shadowOffset: { width: 1, height: 2 },
-    shadowOpacity: 0.9,
-    shadowRadius: 2,
-    elevation: 5,
-    padding: 4,
-  },
-  textContainer: {
-    display: 'flex',
-    alignItems: 'center',
-    flexGrow: 2,
-    borderRadius: 10,
-  },
-  textTitle: {
-    color: 'white',
-    paddingTop: 30,
-    paddingBottom: 10,
-    fontSize: 20,
-    fontFamily: 'Rosario_500Medium',
-  },
-  textAuthor: {
-    color: 'white',
-    paddingHorizontal: 20,
-    fontSize: 15,
-    fontFamily: 'Rosario_300Light_Italic',
   },
 });
